607ed20cde6fbd28a712bb69d85446e0746914d1
?ngel ?lvarez Serra <angel@nan-tic.com>
2013-10-29 17:14:37 +0100

    fix tests

--- tests/test.py
+++ tests/test.py
@@ -22,13 +22,13 @@ class TestCase(unittest.TestCase):
     '''
 
     def setUp(self):
-        trytond.tests.test_tryton.install_module('<module>')
+        trytond.tests.test_tryton.install_module('timetracker')
 
     def test0005views(self):
         '''
         Test views.
         '''
-        test_view('<module>')
+        test_view('timetracker')
 
     def test0006depends(self):
         '''
