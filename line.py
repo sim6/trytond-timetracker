745876e6ae5925d6c68baef409b00fb819273aba
Guillem Barba <guillem@nan-tic.com>
2014-06-11 12:39:33 +0200

    [#020485] Change type of start and end fields to DateTime (with data migration)

--- line.py
+++ line.py
@@ -1,10 +1,14 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import datetime
+from sql import Cast
+from sql.operators import Concat
 
+from trytond import backend
 from trytond.model import ModelView, fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval, Bool
+from trytond.transaction import Transaction
 
 __all__ = ['Line']
 __metaclass__ = PoolMeta
@@ -13,8 +17,12 @@ __metaclass__ = PoolMeta
 class Line:
     __name__ = 'timesheet.line'
 
-    start = fields.Time('Start', states={'readonly': Bool(Eval('hours'))})
-    end = fields.Time('End', states={'readonly': Bool(Eval('hours'))})
+    start = fields.DateTime('Start', states={
+            'readonly': Bool(Eval('hours')),
+            })
+    end = fields.DateTime('End', states={
+            'readonly': Bool(Eval('hours')),
+            })
 
     @classmethod
     def __setup__(cls):
@@ -25,18 +33,63 @@ class Line:
                     },
                 })
 
+    @classmethod
+    def __register__(cls, module_name):
+        TableHandler = backend.get('TableHandler')
+        cursor = Transaction().cursor
+        sql_table = cls.__table__()
+
+        # Migration from 3.0: change start/end field type
+        table = TableHandler(cursor, cls, module_name)
+        migrate_start_end = False
+        if table._columns['start']['typname'] == 'time':
+            migrate_start_end = True
+
+            def get_bak_column_name(column_name):
+                column_bak = column_name + '_bak'
+                bak_it = 0
+                while table.column_exist(column_bak):
+                    column_bak = column_name + '_bak%d' % bak_it
+                    bak_it += 1
+                return column_bak
+
+            start_column_bak = get_bak_column_name('start')
+            table.column_rename('start', start_column_bak, exception=True)
+
+            end_column_bak = get_bak_column_name('end')
+            table.column_rename('end', end_column_bak, exception=True)
+
+        super(Line, cls).__register__(module_name)
+
+        if migrate_start_end:
+            table = TableHandler(cursor, cls, module_name)
+            date_start_bak = Concat(Concat(sql_table.date, ' '),
+                getattr(sql_table, start_column_bak))
+            date_end_bak = Concat(Concat(sql_table.date, ' '),
+                getattr(sql_table, end_column_bak))
+            cursor.execute(*sql_table.update(
+                    columns=[sql_table.start, sql_table.end],
+                    values=[
+                        Cast(date_start_bak, 'timestamp'),
+                        Cast(date_end_bak, 'timestamp'),
+                    ]))
+            table.drop_column(start_column_bak, exception=True)
+            table.drop_column(end_column_bak, exception=True)
+
     @staticmethod
     def default_hours():
         return 0.0
 
     @staticmethod
     def default_start():
-        return datetime.datetime.now().time()
+        return datetime.datetime.now()
 
     @fields.depends('start', 'end')
     def on_change_end(self):
         if self.start and self.end:
-            return {'hours': self._calc_hours(self.end, self.start)}
+            return {
+                'hours': self._calc_hours(self.end, self.start),
+                }
         return {}
 
     @classmethod
